759f1f280ed0522df31e1072f5df71f1901a9b24
faded12 <640826215@qq.com>
2022-06-12 21:14:12 +0800

    updata

--- genMd.js
+++ genMd.js
@@ -4,7 +4,9 @@
     const path = require('path');
 
     const generateInstall = (dirPath = '.') => {
-        const files = [...glob.sync(`${dirPath}/leetcode/*.js`), ...glob.sync(`${dirPath}/function/*.js`)];
+        const leetcodeList = glob.sync(`${dirPath}/leetcode/*.js`)
+        leetcodeList.sort((a,b)=>a.split('/')[2].split('.')[0]-b.split('/')[2].split('.')[0])
+        const files = [...leetcodeList, ...glob.sync(`${dirPath}/function/*.js`)];
         let mdData = ''
         let lcNum = 0
         let fnNum = 0
@@ -17,32 +19,34 @@
         let hrefList = `
 | 🌟 Title 🌟 | 🌟 Title 🌟 |
 | -- | -- |`
-        const listRes = files.map(async (item, index) => {
-            const name = item.split('/')[2]
-            let data = await read(item)
-            let hook = data.substring(data.lastIndexOf('{') + 1, data.lastIndexOf('}')).length > 10 ? '🔘' : '⚪️';
-            (/[\u4e00-\u9fa5]/).test(item) ? lcNum++ : fnNum++
-            if (flag) {
-                hrefList += `
+        async function listRes(){
+            for(let item of files){
+                const name = item.split('/')[2]
+                let data = await read(item)
+                let hook = data.substring(data.lastIndexOf('{') + 1, data.lastIndexOf('}')).length > 10 ? '🔘' : '⚪️';
+                (/[\u4e00-\u9fa5]/).test(item) ? lcNum++ : fnNum++
+                if (flag) {
+                    hrefList += `
 | `
-            }
-            hrefList += ` ${hook}    <a href="#${name.split('.').join('')}">${name}</a> |`
-            flag = !flag
-            mdData += `
-        
+                }
+                hrefList += ` ${hook}    <a href="#${name.split('.').join('')}">${name}</a> |`
+                flag = !flag
+                mdData += `
+            
 ## ${name}
 \`\`\` javascript
 ${data}
 \`\`\`
 <a href="#开始">回到目录</a>
 `
-            mdData += ``
-        })
+            }
+        }
 
-        Promise.all(listRes).then(() => {
+        listRes().then(() => {
             headInfo += `
 ##### 🤯 LeetCode：${lcNum}
-##### 🧐 JS方法实现：${fnNum}`
+##### 🧐 JS方法实现：${fnNum}
+`
             let data = headInfo + hrefList + mdData
             fs.writeFile(path.join(__dirname, `./README.md`), data, 'utf8', (err) => {
                 if (err) throw err;
