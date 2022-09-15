498cc3426569d044fe5f5065415a48f14e0b9a6b
Faded12 <640826215@qq.com>
2022-09-15 23:59:25 +0800

    updata

---
+++ leetcode/1617.统计子树中城市之间最大距离.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=1617 lang=javascript
+ *
+ * [1617] 统计子树中城市之间最大距离
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number[]}
+ */
+var countSubgraphsForEachDiameter = function(n, edges) {
+
+};
+// @lc code=end
+
